8e32c24b2ee45eb9eb76a2d226d2e8d0a8c77d60
niuyan <niuyan_i@didiglobal.com>
2020-12-23 14:19:09 +0800

    ini

---
+++ LibA/LibA.m
@@ -0,0 +1,15 @@
+//
+//  LibA.m
+//  LibA
+//
+//  Created by didiniuyan on 2020/12/22.
+//  Copyright © 2020 didi. All rights reserved.
+//
+
+#import "LibA.h"
+
+@implementation LibA
+- (void)printLib{
+    NSLog(@"LibA方法在运行");
+}
+@end
